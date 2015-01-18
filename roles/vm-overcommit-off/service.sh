79d4c7ac28dd1e7ab80614ee9d2d119e88ad88bd
millerlogic <chris@cmiller.me>
2015-01-18 15:45:04 -0800

    Commit = RAM+Swap

--- roles/vm-overcommit-off/service.sh
+++ roles/vm-overcommit-off/service.sh
@@ -6,11 +6,12 @@
 # If using this role, consider also using swap.
 
 if [ x"$1" == x"install" ] || [ x"$1" == x"start" ]; then
-	sysctl -w vm.overcommit_memory=2 &&
-		sysctl -w vm.overcommit_ratio=200 &&
-		echo Disabled vm.overcommit_memory
+	sysctl -w vm.overcommit_memory=0 # Keeping it 0.
+	sysctl -w vm.overcommit_ratio=0 &&
+		echo "Overcommit ratio = 0"
 elif [ x"$1" == x"status" ]; then
 	sysctl vm.overcommit_memory
+	sysctl vm.overcommit_ratio
 elif [ x"$1" == x"stop" ]; then
 	# Setting the default value:
 	sysctl -w vm.overcommit_memory=0

5872bae1366130246ccf2338a5357f45d4be8565
millerlogic <chris@cmiller.me>
2015-01-18 15:45:49 -0800

    Pass args along

--- roles/testbed/service.sh
+++ roles/testbed/service.sh
@@ -3,10 +3,12 @@
 # Gives you a testbed contanier for testing new potential roles.
 # Warning: if you use docker in the testbed container, it accesses the host's docker!
 
+action="$1"
 cname=testbed
 imgname="rastasheep/ubuntu-sshd:14.04"
+shift; shift; shift;
 
-$BASE_DIR/roles/docker-container/service-immutable.sh "$1" $cname $imgname \
-    -p 127.0.0.1:1022:22 -v /var/run/docker.sock:/var/run/docker.sock
+$BASE_DIR/roles/docker-container/service-immutable.sh "$action" $cname $imgname \
+    -p 127.0.0.1:1022:22 -v /var/run/docker.sock:/var/run/docker.sock "$@"
 
 echo Use: ssh root@localhost -p 1022

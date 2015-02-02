fafcb750717ee3aa09cd1466b830fe94cbdb8092
millerlogic <chris@cmiller.me>
2015-02-01 16:30:45 -0800

    Let the deploy user control docker

--- roles/docker-debian-7/install.sh
+++ roles/docker-debian-7/install.sh
@@ -5,6 +5,9 @@ if ! command -v docker; then
 	echo | curl -sSL https://get.docker.com/ | sh
 fi
 
+# Let the deploy user control docker.
+usermod -a -G docker deploy || true
+
 # Setup standard user for containers.
 groupadd -g 28101 container || echo
 useradd -u 28101 -N -g 28101 container || echo
